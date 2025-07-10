4abd5ecac946dff8c5be1021eb405148fdbd0b33
OmgRod <89850217+OmgRod@users.noreply.github.com>
2025-07-10 16:15:36 +0100

    fix

--- build.lua
+++ build.lua
@@ -3,7 +3,7 @@ return {
   developer = 'OmgRod',
   output = 'dist',
   version = 'v1.0.0-alpha.1',
-  love = '12',
+  love = '12.0',
   ignore = {'dist', '.github', 'aseprite', '.gitignore', 'README.md'},
   icon = 'icons/icon256.png',
   use32bit = true,
